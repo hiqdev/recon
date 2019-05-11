436165aeded1a604d92364ef92e547ab3fffd1d1
Andrii Vasyliev <sol@hiqdev.com>
2019-05-11 17:30:35 +0300

    Changed config to be yii 2/3 compatible

--- config/common.php
+++ config/common.php
@@ -1,52 +1,66 @@
 <?php
 
+use hiqdev\yii\compat\yii;
+
 $params = $params ?? [];
 
-return [
-    'container' => [
-        'singletons' => [
-            \hiqdev\recon\core\Service\AmqpBindingsProvider::class => [
-                '__class' => \hiqdev\recon\core\Service\AmqpBindingsProvider::class,
-            ],
+$app = [
+    'controllerMap' => [
+        'queue' => [
+            '__class' => \hiqdev\recon\core\Console\QueueController::class,
+        ],
+    ],
+];
 
-            /// Connectivity
-            \PhpAmqpLib\Connection\AMQPStreamConnection::class => [
-                '__class' => \PhpAmqpLib\Connection\AMQPLazyConnection::class,
-                '__construct()' => [
-                    $params['amqp.host'],
-                    $params['amqp.port'],
-                    $params['amqp.user'],
-                    $params['amqp.password']
-                ],
-            ],
+$singletons = [
+    \hiqdev\recon\core\Service\AmqpBindingsProvider::class => [
+        '__class' => \hiqdev\recon\core\Service\AmqpBindingsProvider::class,
+    ],
 
-            'recon.queue' => [
-                '__class' => \hiqdev\yii2\autobus\components\SingleCommandAutoBus::class,
-                '__construct()' => [
-                    [
-                        '__class' => \hiqdev\recon\core\Command\IncomingTask::class,
-                    ]
-                ],
-            ],
+    /// Connectivity
+    \PhpAmqpLib\Connection\AMQPStreamConnection::class => [
+        '__class' => \PhpAmqpLib\Connection\AMQPLazyConnection::class,
+        '__construct()' => [
+            $params['amqp.host'],
+            $params['amqp.port'],
+            $params['amqp.user'],
+            $params['amqp.password']
+        ],
+    ],
 
-            \hiqdev\recon\core\Service\TaskLifecycle::class => [
-                '__class' => \hiqdev\recon\core\Service\TaskLifecycle::class,
-                '__construct()' => [
-                    \hiqdev\yii\compat\yii::referenceTo('task-lifecycle-emitter'),
-                ],
-            ],
+    'recon.queue' => [
+        '__class' => \hiqdev\yii2\autobus\components\SingleCommandAutoBus::class,
+        '__construct()' => [
+            [
+                '__class' => \hiqdev\recon\core\Command\IncomingTask::class,
+            ]
+        ],
+    ],
 
-            /// Request handling
-            'task-lifecycle-emitter' => [
-                '__class' => \hiapi\event\ConfigurableEmitter::class,
-                'listeners' => [
+    \hiqdev\recon\core\Service\TaskLifecycle::class => [
+        '__class' => \hiqdev\recon\core\Service\TaskLifecycle::class,
+        '__construct()' => [
+            \hiqdev\yii\compat\yii::referenceTo('task-lifecycle-emitter'),
+        ],
+    ],
+
+    /// Request handling
+    'task-lifecycle-emitter' => [
+        '__class' => \hiapi\event\ConfigurableEmitter::class,
+        'listeners' => [
 // Example: add your listener for events, described in TaskLifecycle class
 //                    [
 //                        'event' => \hiqdev\recon\core\Service\TaskLifecycle::EVENT_RESOLVE_TASK,
 //                        'listener' => \hiqdev\recon\core\Service\TaskResolver::class,
 //                    ],
-                ],
-            ],
-        ]
-    ]
+        ],
+    ],
 ];
+
+return yii::is2() ? array_merge([
+    'container' => [
+        'singletons' => $singletons,
+    ],
+], $app) : array_merge([
+    'app' => $app,
+], $singletons);
